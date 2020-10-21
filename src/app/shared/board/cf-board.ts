346ba8fe8f885c828befd220b3270f68701d8202
hojat <fardinho@msu.edu>
2020-10-21 12:44:52 -0400

    first commit on new branch

--- src/app/shared/board/cf-board.ts
+++ src/app/shared/board/cf-board.ts
@@ -3,7 +3,11 @@ import { Board } from "./abstract.board";
 export class ConnectFourBoard extends Board{
 
     constructor(canvas : HTMLCanvasElement) {
-        super(canvas, 7, 6);
+        super(canvas, 6, 7);
+        this.emptySpots = 42;
+        this.unsetAndEmptySpots = 42;
+        this.drawBoardAndPieces();
+
     }
 
     checkForWinner() : string{
@@ -15,7 +19,64 @@ export class ConnectFourBoard extends Board{
     }
 
     drawBoard() {
+
+
+        const widthOfOneSpot = this.canvas.width/this.lengthOfRow;
+        const heightOfOneSpot = this.canvas.height/this.lengthOfCol
+
+        this.context.beginPath()
+        this.context.rect(0,0, this.canvas.width, this.canvas.height)
+        this.context.fillStyle = "#5d8ed0"
+        this.context.fill()
+
         
+
+
+        let i = this.canvas.width/this.lengthOfRow
+        while (i < this.canvas.width) {
+            this.context.beginPath() 
+            this.context.moveTo(i,0);
+            this.context.lineTo(i,this.canvas.height);
+            i+=this.canvas.width/this.lengthOfRow;
+            this.context.stroke()
+        }
+
+        i = this.canvas.height/this.lengthOfCol
+
+        //draw rows
+        while (i < this.canvas.height) {
+            this.context.beginPath() 
+            this.context.moveTo(0,i);
+            this.context.lineTo(this.canvas.width,i);
+            i+=this.canvas.height/this.lengthOfCol;
+            this.context.stroke()
+        } 
+
+        i = 0;
+        let j = 0;
+
+
+        while (true) {
+
+            this.context.beginPath();
+            this.context.fillStyle = "white";
+            this.context.ellipse(
+                widthOfOneSpot*(j+0.5),
+                heightOfOneSpot*(i+0.5),
+                widthOfOneSpot*0.25,
+                heightOfOneSpot*0.25,0,0,Math.PI*2
+            );
+            this.context.fill();
+            
+            i++;
+            if (i > this.lengthOfCol - 1) {
+                i = 0;
+                j++;
+            } 
+            if (j > this.lengthOfRow - 1) {
+                break;
+            }
+        }
     }
 
     drawPieces() {
